25e389ab9d7ec57088553ece2af96e7fcfd6df5c
Allan James Lapid <ajLapid718@gmail.com>
2017-11-09 20:52:21 -0500

    Require highline gem && get email plus password

--- Unlike_Procedure.rb
+++ Unlike_Procedure.rb
@@ -1,1 +1,21 @@
-# Placeholder
+require 'highline/import'
+
+def get_email
+  print "Enter your e-mail address: "
+  return gets.chomp
+end
+
+def get_password
+  begin
+    facebook_password = ask("Enter your password: ") { |q| q.echo = "*" }
+    password_confirmation = ask("Confirm your password: ") { |q| q.echo = "*" }
+    if facebook_password == password_confirmation
+      return facebook_password
+    else
+      raise Exception.new("Your passwords do not match. Try again.")
+    end
+  rescue Exception => error_message
+    puts error_message
+    retry
+  end
+end
